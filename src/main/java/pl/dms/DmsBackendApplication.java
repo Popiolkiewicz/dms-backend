a8efd99741e0dca7b883535a67d5e89ce91fb5a2
Hubert Popiołkiewicz <popiolkiewicz.it@gmail.com>
2019-06-28 18:45:52 +0200

    Initial commit

---
+++ src/main/java/pl/dms/DmsBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmsBackendApplication.class, args);
+	}
+
+}
