3843d49358e71d629128068a3dd04c9091d42d45
Hubert Popiołkiewicz <popiolkiewicz.it@gmail.com>
2019-07-04 19:05:56 +0200

    #: Developing OAuth2

--- src/main/java/pl/dms/Constants.java
+++ src/main/java/pl/dms/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
 
     public static final String API_URL = "/api";
 
+    public static final String JWT_SIGNING_KEY = "123";
+
 }
--- src/main/java/pl/dms/config/WebSecurityConfig.java
+++ src/main/java/pl/dms/config/WebSecurityConfig.java
@@ -2,7 +2,9 @@ package pl.dms.config;
 
 import java.util.Arrays;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,6 +36,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .withUser("user")
             .password("password")
             .roles("USER");
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     private CorsConfigurationSource createCordConfigurationSource() {
